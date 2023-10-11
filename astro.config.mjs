3b5588039c21a8d059af223648f23d41689dfa6f
Elvann <elvann.abendroth@gmail.com>
2023-10-11 19:36:33 -0400

    👋init

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,36 @@
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from 'astro/config'
+import starlight from '@astrojs/starlight'
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', link: '/guides/example/' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
-});
+  integrations: [
+    starlight({
+      title: 'In a Noteshell',
+      social: {
+        github: 'https://github.com/withastro/starlight',
+      },
+      sidebar: [
+        {
+          label: 'Introduction',
+          autogenerate: { directory: 'introduction' },
+        },
+        // {
+        //   label: 'Counterpoint',
+        //   autogenerate: { directory: 'counterpoint' },
+        // },
+        {
+          label: 'Basic Harmony',
+          autogenerate: { directory: 'harmony-1' },
+        },
+        // {
+        //   label: 'Advanced Harmony',
+        //   autogenerate: { directory: 'harmony-2' },
+        // },
+        // {
+        //   label: 'References',
+        //   autogenerate: { directory: 'reference' },
+        // },
+      ],
+    }),
+  ],
+})
---
+++ src/content/docs/introduction/start-here.md
@@ -0,0 +1,6 @@
+---
+title: 'Start Here!'
+description: A guide in my new Starlight docs site.
+---
+
+Welcome to a music theory resource!
