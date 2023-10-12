2c618472bf8252b4f68274ab5887b6d961affd61
Elvann <elvann.abendroth@gmail.com>
2023-10-11 20:15:45 -0400

    ✏️content

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,14 +14,33 @@ export default defineConfig({
           label: 'Introduction',
           autogenerate: { directory: 'introduction' },
         },
+        {
+          label: 'Basic Music Theory',
+          autogenerate: { directory: 'basic-theory' },
+        },
         // {
         //   label: 'Counterpoint',
         //   autogenerate: { directory: 'counterpoint' },
         // },
         {
           label: 'Basic Harmony',
-          autogenerate: { directory: 'harmony-1' },
+          // autogenerate: { directory: 'harmony-1' },
+          items: [
+            {
+              label: '1. Introduction to Harmony',
+              autogenerate: { directory: 'basic-harmony/chapter-01' },
+            },
+            {
+              label: '2. Composing in Minor Keys',
+              autogenerate: { directory: 'basic-harmony/chapter-02' },
+            },
+            {
+              label: '3. Harmonizing a Melody',
+              autogenerate: { directory: 'basic-harmony/chapter-03' },
+            },
+          ],
         },
+
         // {
         //   label: 'Advanced Harmony',
         //   autogenerate: { directory: 'harmony-2' },
